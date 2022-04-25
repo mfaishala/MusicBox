9aa91cf65ee37b7f38dba752fee676fc79c444bd
Muhammad Faishal A <mfaishalalim1@gmail.com>
2022-04-26 00:04:10 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/musicbox/All_Albums.java
@@ -0,0 +1,14 @@
+package com.example.musicbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class All_Albums extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_albums);
+    }
+}
---
+++ app/src/main/java/com/example/musicbox/Liked_Song.java
@@ -0,0 +1,14 @@
+package com.example.musicbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Liked_Song extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liked_song);
+    }
+}
